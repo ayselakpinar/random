0f05c6ee2707d45846af0adfb6489dba7d054202
ayselakpinar <ayselesya96@gmail.com>
2025-02-13 21:02:27 +0100

    created a card

---
+++ random-app/src/Card.jsx
@@ -0,0 +1,14 @@
+import catImage from "./cat.jpg";
+function Card() {
+  return (
+    <div>
+      <img src={catImage} />
+      <h2>Aysel</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum, quia?
+      </p>
+    </div>
+  );
+}
+
+export default Card;
--- random-app/src/index.js
+++ random-app/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Card from './Card.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Card/>
   </React.StrictMode>
 );
 
